e5d1c8d5e4678d00230ecc96deb1cf94fe765aea
EriksK82 <eriks.karklins82@gmail.com>
2023-02-11 22:29:57 +0200

    P2_2

--- Praktiskie darbi/PD_03_2/PD_03_2/main.cpp
+++ Praktiskie darbi/PD_03_2/PD_03_2/main.cpp
@@ -3,12 +3,21 @@
 //  PD_03_2
 //
 //  Created by Eriks Karklins on 11/02/2023.
-//
+//Izveidot klasi "Rectangle", ar laukiem platums un garums. Konstruktorā inicializēt  parametru sākumvērtības. Jārealizē sekojošas funkcijas:
+//Konstruktors
+//SetRectangle
+//PrintRectangle (izvada abus parametrus)
+
+//kā arī:
+//Length (atrod garāko malu)
+//Width (atrod īsāko malu)
+//Perimeter
+//Area
+//IsSquare (vai ir kvadrāts?)
+
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main() {
+
 }
