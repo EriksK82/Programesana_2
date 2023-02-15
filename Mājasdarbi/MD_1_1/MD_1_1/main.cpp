2c6b530e82131c0ceacb41ad0d0182762fd7d1c5
EriksK82 <eriks.karklins82@gmail.com>
2023-02-15 20:45:59 +0200

    MD_1_1

---
+++ Mājasdarbi/MD_1_1/MD_1_1/main.cpp
@@ -0,0 +1,41 @@
+//
+//  main.cpp
+//  MD_1_1
+//
+//  Created by Eriks Karklins on 14/02/2023.
+
+#include <iostream>
+#include "Header.h"
+
+using namespace std;
+
+
+
+int main()
+{
+    
+    Character Ch1("Eriks", 10);//Testa personāžs Nr.1 (vards,dzivibas)
+    int Ch1_HitsTaken = 4;//samazina personāža dzīvi par parametra vērtību
+    int Ch1_MoveCount = 5;// personāža move skaits
+    
+    Ch1.Go(Ch1_MoveCount);// Go parbauda vai ievadits korekti, liek ievadīt kustības virzienus pēc skaita;
+    Ch1.Hit(Ch1_HitsTaken);//statuss dead or alive
+    Ch1.GetLife();//izvada aktuālo dzīvību skaitu
+    Ch1.PrintCharacter();// informacijas izvade Vards, dzivibas, gajieni......
+  
+    
+    Character Ch2("Katrina", 7);
+    int Ch2_HitsTaken = 7;
+    int Ch2_MoveCount = 7;
+
+    Ch2.Go(Ch2_MoveCount);
+    Ch2.Hit(Ch2_HitsTaken);
+    Ch2.GetLife();
+    Ch2.Hit(Ch2_HitsTaken);
+    Ch2.PrintCharacter();
+
+   cout << endl;
+
+   return 0;
+}
+
