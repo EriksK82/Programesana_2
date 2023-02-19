d07ed68eb605dd6b38eaaed24ed3c99a9a8c1b85
EriksK82 <eriks.karklins82@gmail.com>
2023-02-19 17:43:19 +0200

    2_4

--- Mājasdarbi/MD_1_2/MD_1_2/main.cpp
+++ Mājasdarbi/MD_1_2/MD_1_2/main.cpp
@@ -15,11 +15,23 @@ using namespace std;
 int main() {
 
     
-    iRobot R1("1.modelis","Left_sen",false, "Right_sen",false);
+    iRobot R1("1.modelis","Left_sen",false, "Right_sen",false);// 1.robota nosaukums, kreisā sensora stāvoklis, labā sensorastāvoklis
+    int room_R1[10] = {23, 35, 55, 74, 41, 43, 34, 77, 23, 98};// 1.robota room
     
-    int room_R1[10] = {23, 35, 55, 74, 45, 47, 34, 77, 23, 98};
+    iRobot R2("2.modelis","Left_sen",false, "Right_sen",false);
+    int room_R2[10] = {23, 35, 55, 74, 44, 47, 34, 77, 23, 98};
     
-    R1.Run(room_R1);
-    R1.PrintRobot();
+    
+    R1.SwitchOnOff();// ieslēdzam sensorus ja izslēgti state falses mainam uz true
+    R1.Run(room_R1);// laižam pa room
+    
+    R2.SwitchOnOff();
+    R2.Run(room_R2);
+    
+    R1.PrintRobot();// izvadam informāciju par robotu
+    R2.PrintRobot();
+
+    
+    
 
 }
