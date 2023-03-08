bb8bdc181694227de922a35f63b416b0c71dd81d
EriksK82 <eriks.karklins82@gmail.com>
2023-03-08 17:47:24 +0200

    2_1

---
+++ Mājasdarbi/MD_2_1/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+cout <<"gatavojamies 2MD bet jau ar VSC"<< endl;
+
+string teksts;
+
+getline(cin, teksts);
+
+cout <<"izveidotais teksts: "<<teksts<<endl;
+
+
+
+}
+
+
