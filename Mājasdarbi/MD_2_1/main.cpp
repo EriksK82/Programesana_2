68e60041e4f909f016af93cb5e34bf411fcf93b1
EriksK82 <eriks.karklins82@gmail.com>
2023-04-03 18:40:50 +0300

    2MD

--- Mājasdarbi/MD_2_1/main.cpp
+++ Mājasdarbi/MD_2_1/main.cpp
@@ -10,84 +10,346 @@
 // laika samazināšana par vienu minūti ar -- postfiksa (x--) operāciju (ja tas ir minūtes pirmā vērtība, tad jāsamazina arī stundu, u.t.t.)
 // laika salīdzināšana ar >, <, >=, <=, ==, != operatoriem. Jāatgriež bool vērtību.
 
-#include <iostream>
+#include "iostream"
 
 using namespace std;
 
-class Time
-{
+class Time {
+    
+    friend istream& operator>>(istream&, Time&);
+    friend ostream& operator<<(ostream&, const Time&);
+    
+
 public:
-   Time();
-   void setTime(int, int, int);
-   void printTime();
-   Time &setH(int);
-   Time &setM(int);
-   Time &setS(int);
+    Time(int=0, int=0, int=0);
+    Time& operator=(const Time&);
+    Time operator++();
+    Time operator++(int);
+    Time operator--();
+    Time operator--(int);
+    bool operator>(const Time&) const;
+    bool operator<(const Time&) const;
+    bool operator>=(const Time&) const;
+    bool operator<=(const Time&) const;
+    bool operator==(const Time&) const;
+    bool operator!=(const Time&) const;
 
 private:
-   int hour;   // 0-23
-   int minute; // 0-59
-   int second; // 0-59
+    int hours;
+    int minutes;
+    int seconds;
 };
 
+// konstruktors ar noklusēto laika vērtību
+Time::Time(int h, int m, int s) { 
 
-Time::Time()
-{
-   hour=minute=second = 0;
+    hours = (h>=0 && h <23) ? h : 0;// ja lielāks par 23 tad 0
+    minutes = (m>=0 && m<59) ? m : 0;// ja lielāks par 59 tad 0
+    seconds = (s>=0 && s<59) ? s : 0;//ja lielāks par 59 tad 0
 }
 
-Time &Time::setH(int a)
-{
-   hour=a;
-   return *this;
+// laika ievade (ar >>)
+istream& operator>>(istream& input, Time& obj) {
+    string inputH, inputM, inputS;
+
+    cout << "Enter hours : ";
+    cin >> inputH;
+
+    //Ievades parbaude Hours
+    int i = 0;
+    while (i < inputH.length()) {
+        if (isdigit(inputH[i]) == false || stoi(inputH) < 0 || stoi(inputH) > 23) { // parbaudam vai ievade: == ir skaitlis, vai nav < par 0,vai nav > par 23//stoi jo inputx ir string
+            cout << "Wrong enttry hours : "<< endl; // ja izpildas ludzam ievadīt atkartoti
+            cout << "Enter hours : "; 
+            input >> inputH;
+            i = 0;
+        }
+
+        i++;
+    }
+
+    cout << "Enter minutes: ";
+    cin >> inputM;
+
+    //Ievades parbaude minutes
+    i=0;
+    while (i < inputM.length()) {
+        if (isdigit(inputM[i]) == false || stoi(inputM) < 0 || stoi(inputM) > 59) { // parbaudam vai ievade: == ir skaitlis, vai nav < par 0,vai nav > par 59
+            cout << "Wrong enttry minutes: "<< endl; // ja izpildas ludzam ievadīt atkartoti
+            cout << "Enter minutes: ";
+            input >> inputM;
+            i = 0;
+        }
+
+        i++;
+    }
+
+    cout << "Enter seconds: ";
+    cin >> inputS;
+
+    //Ievades parbaude seconds
+    i=0;
+    while (i < inputS.length()) {
+        if (isdigit(inputS[i]) == false || stoi(inputS) < 0 || stoi(inputS) > 59) { // parbaudam vai ievade: == ir skaitlis, vai nav < par 0,vai nav > par 59
+            cout << "Wrong enttry seconds: "<< endl; // ja izpildas ludzam ievadīt atkartoti
+            cout << "Enter seconds: ";
+            input >> inputS;
+            i = 0;
+        }
+
+        i++;
+    }
+
+//Vērtibu pieskirsana klases Time objektiem
+    obj.hours = stoi(inputH);
+    obj.minutes = stoi(inputM);
+    obj.seconds = stoi(inputS);
+
+    return input;
 }
 
-Time &Time::setM(int a)
-{
-   minute=a;
-  // return *this;
+// laika izvade (ar <<)
+ostream& operator<<(ostream& output, const Time& obj) {
+    output
+    //no lekcijas piemēra - ja viens simbols pieliekam 0 kā arī starpā saliekam ":"
+            << (obj.hours < 10 ? "0" : "") << obj.hours << ":"
+            << (obj.minutes < 10 ? "0" : "") << obj.minutes << ":"
+            << (obj.seconds < 10 ? "0" : "") << obj.seconds
+            <<
+            endl;
+
+    return output;
 }
 
-Time &Time::setS(int a)
-{
-   second=a;
-   return *this;
+
+//laika piešķiršana citam laikam
+Time& Time::operator=(const Time& right) {
+    if (*this == right) {
+        return *this;
+    }
+
+    hours = right.hours;
+    minutes = right.minutes;
+    seconds = right.seconds;
+
+    return *this;
 }
 
-void Time::setTime (int h, int m, int s)
-{
-  hour = (h>=0 && h <24) ? h : 0;
-  minute = (m>=0 && m<60) ? m : 0;
-  second = (s>=0 && s<60) ? s : 0;
+//laika palielināšana par vienu sekundi ar ++ prefiksa (++x) operāciju (ja tas ir sekundes pēdēja vērtība, tad jāpalielina arī minūti, u.t.t.)
+Time Time::operator++() {
+    if (seconds == 59) {
+        if (minutes == 59) {
+            if (hours == 23) {
+                hours = 0;
+            } else {
+                hours++;
+            }
+            minutes = 0;
+        } else {
+            minutes++;
+        }
+        seconds = 0;
+    } else {
+        seconds++;
+    }
+
+    return *this;
 }
 
-void Time::printTime()
-{
-   cout << (hour < 10 ? "0" : "") << hour << ":"
-		<< (minute < 10 ? "0" : "") << minute << ":"
-		<< (second < 10 ? "0" : "") << second;
+//laika palielināšana par vienu minūti ar ++ postfiksa (x++) operāciju (ja tas ir minūtes pēdēja vērtība, tad jāpalielina arī stundu, u.t.t.)
+Time Time::operator++(int) {
+    if (minutes == 59) {
+        if (hours == 23) {
+            hours = 0;
+        } else {
+            hours++;
+        }
+
+        minutes = 0;
+    } else {
+        minutes++;
+    }
+
+    return *this;
+
 }
 
-int main()
-{
-   
-   Time t;
-   cout << "Laiks ir: ";
-   t.printTime();
-   t.setTime(13, 27, 6);
-   cout << endl << "Laiks ir: ";
-   t.printTime();
+//laika samazināšana par vienu sekundi ar -- prefiksa (--x) operāciju (ja tas ir sekundes pirmā vērtība, tad jāsamazina arī minūti, u.t.t.)
+Time Time::operator--() {
+    if (seconds == 0) {
+        if (minutes == 0) {
+            if (hours == 0) {
+                hours = 23;
+            } else {
+                hours--;
+            }
 
-   t.setTime(78, 10, 99);
+            minutes = 59;
+        } else {
+            minutes--;
+        }
 
-   cout << endl << "Peec nepareiza meeginaajuma: ";
-   t.printTime();
-   cout << endl << endl;
-   t.setH(5).setS(5).setM(5).setH(6);
-   t.printTime();
-   cout << endl << endl;
-   system("Pause");
-   return 0;
+        seconds = 59;
+    } else {
+        seconds--;
+    }
+
+    return *this;
+}
+
+//laika samazināšana par vienu minūti ar -- postfiksa (x--) operāciju (ja tas ir minūtes pirmā vērtība, tad jāsamazina arī stundu, u.t.t.)
+Time Time::operator--(int) {
+    if (minutes == 0) {
+        if (hours == 0) {
+            hours = 23;
+        } else {
+            hours--;
+        }
+
+        minutes = 59;
+    } else {
+        minutes--;
+    }
+
+    return *this;
+}
+
+
+//laika salīdzināšana ar >
+bool Time::operator>(const Time &right) const {
+    if (hours > right.hours) {
+        return true;
+    }
+
+    if (hours == right.hours) {
+        if (minutes > right.minutes) {
+            return true;
+        }
+
+        if (minutes == right.minutes && seconds > right.seconds) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+//laika salīdzināšana ar <
+bool Time::operator<(const Time &right) const {
+        if (hours < right.hours) {
+        return true;
+    }
+
+    if (hours == right.hours) {
+        if (minutes < right.minutes) {
+            return true;
+        }
+
+        if (minutes == right.minutes && seconds < right.seconds) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+//laika salīdzināšana ar >=
+bool Time::operator>=(const Time &right) const {
+    if (*this > right || *this == right) {
+        return true;
+    }
+
+    return false;
+}
+
+
+//laika salīdzināšana ar <=
+bool Time::operator<=(const Time &right) const {
+    if (*this < right || *this == right) {
+        return true;
+    }
+
+    return false;
+}
+
+//laika salīdzināšana ar ==
+bool Time::operator==(const Time &right) const {
+    if (hours == right.hours &&
+        minutes == right.minutes &&
+        seconds == right.seconds) {
+        return true;
+    }
+
+    return false;
+}
+
+
+//laika salīdzināšana ar !=
+bool Time::operator!=(const Time &right) const {
+    return !(*this == right);
+}
+
+
+int main() {
+
+// sākotnējā laika uzstādīšana
+    Time laiks1(14,25,25), laiks2(1,9,2), laiks3(25,60,60);
+
+//sākotnējā laika izvade
+    cout << "Set time:"<<endl << "Laiks 1: " << laiks1 << endl<< "Laiks 2: " << laiks2<< endl << "Laiks 3: " << laiks3 << endl;
+
+//laika ievade
+    cin>>laiks3;
+
+//laika izvade
+    cout << "Laiks 3: " << laiks3 << endl;
+
+//laika piešķiršana citam laikam, ar iespēju laiks1=laiks2=laiks3
+    laiks1=laiks2=laiks3;
+
+//izvade pēc laika piešķiršanas citam laikam
+    cout << "time after laiks1=laiks2=laiks3:"<<endl << "Laiks 1: " << laiks1 << endl<< "Laiks 2: " << laiks2<< endl << "Laiks 3: " << laiks3 << endl;
+
+//laika palielināšana par vienu sekundi ar ++ prefiksa 
+    ++ laiks1;
+
+//laika izvade
+    cout << "++ Laiks 1: " << laiks1 << endl;
+
+//laika palielināšana par vienu minūti ar ++ postfiksa
+    laiks1 ++;
+
+//laika izvade
+    cout << "Laiks 1 ++: " << laiks1 << endl;
+
+//laika samazinasana par vienu sekundi ar -- prefiksa 
+    -- laiks1;
+
+//laika izvade
+    cout << "-- Laiks 1: " << laiks1 << endl;
+
+//laika samazinašana par vienu minūti ar -- postfiksa
+    laiks1 --;
+
+//laika izvade
+    cout << "Laiks 1 --: " << laiks1 << endl;
+
+//kādi tagad laiki
+    cout << "Current times:"<<endl << "Laiks 1: " << laiks1 << endl<< "Laiks 2: " << laiks2<< endl << "Laiks 3: " << laiks3 << endl;
+
+//laika salīdzināšana ar >, <, >=, <=, ==, != operatoriem
+if (laiks1 > laiks2)
+        cout << "laiks 1 > laiks 2" << endl;
+    if (laiks1 < laiks2)
+        cout << "laiks 1 < laiks 2" << endl;
+    if (laiks1 >= laiks2)
+        cout << "laiks 1 >= laiks 2" << endl;
+    if (laiks1 <= laiks2)
+        cout << "laiks 1 <= laiks 2" << endl;
+    if (laiks1 == laiks2)
+        cout << "laiks 1 == laiks 2" << endl;
+    if (laiks1 != laiks2)
+        cout << "laiks 1 != laiks2" << endl;
 }
 
 
--- Mājasdarbi/MD_2_2/main.cpp
+++ Mājasdarbi/MD_2_2/main.cpp
@@ -11,11 +11,261 @@
 // Galvenā programmā nodemonstrēt visu operatoru darbu.
 // Lai dabūt 10 masīvu ar vektora koordinātēm jāveido dinamiski!
 
-#include <iostream>
+#include "iostream"
+#include "cmath"
 
 using namespace std;
 
-int main(){
-    
-    cout<<"no clue what to do"<<endl;
+class Vector {
+    friend ostream &operator<<(ostream &, const Vector &);
+    friend istream &operator>>(istream &, Vector &);
+
+public:
+    Vector(double = 0, double = 0, double = 0, double = 0, double = 0, double = 0);
+    ~Vector();
+    Vector &operator=(const Vector &);
+    Vector operator+(const Vector &) const;
+    const Vector operator-(const Vector &) const;
+    const Vector operator*(const Vector &) const;
+    bool operator>(const Vector &) const;
+    bool operator<(const Vector &) const;
+    bool operator>=(const Vector &) const;
+    bool operator<=(const Vector &) const;
+    bool operator==(const Vector &) const;
+    bool operator!=(const Vector &) const;
+    double getMagnitude() const;
+
+private:
+    double* coordinates = new double[6];
+};
+
+//pauze
+
+// konstruktors pieskir parmetru vertibas masiva
+Vector::Vector(double x1, double y1, double z1, double x2, double y2, double z2) {
+    coordinates[0] = x1;
+    coordinates[1] = y1;
+    coordinates[2] = z1;
+    coordinates[3] = x2;
+    coordinates[4] = y2;
+    coordinates[5] = z2;
 }
+
+Vector::~Vector() { // destruktors deallocate masivu
+    delete [] coordinates;
+}
+
+ostream &operator<<(ostream &output, const Vector &obj) {
+    output << "x1: " << obj.coordinates[0] << "\n"
+          << "y1: " << obj.coordinates[1] << "\n"
+          << "z1: " << obj.coordinates[2] << "\n"
+          << "x2: " << obj.coordinates[3] << "\n"
+          << "y2: " << obj.coordinates[4] << "\n"
+          << "z2: " << obj.coordinates[5]
+          << endl;
+
+    return output;
+}
+
+istream &operator>>(istream &input, Vector &obj) {
+    double x1, y1, z1, x2, y2, z2;
+
+    cout << "Enter x1: ";
+    input >> x1;
+    obj.coordinates[0] = x1;
+
+    cout << "Enter y1: ";
+    input >> y1;
+    obj.coordinates[1] = y1;
+
+    cout << "Enter z1: ";
+    input >> z1;
+    obj.coordinates[2] = z1;
+
+    cout << "Enter x2: ";
+    input >> x2;
+    obj.coordinates[3] = x2;
+
+    cout << "Enter y2: ";
+    input >> y2;
+    obj.coordinates[4] = y2;
+
+    cout << "Enter z2: ";
+    input >> z2;
+    obj.coordinates[5] = z2;
+
+    return input;
+}
+// dabujam vektora magnitudu pec formulas sqrt(a^2+b^2+c^2)
+// a = x2 - x1, b = y2 - y1, c = z2 - z1
+double Vector::getMagnitude() const {
+    return sqrt(pow(coordinates[3] - coordinates[0], 2) +
+            pow(coordinates[4] - coordinates[1], 2) +
+            pow(coordinates[5] - coordinates[2], 2));
+}
+
+Vector &Vector::operator=(const Vector &right) {
+    if (*this == right) {
+        return *this;
+    }
+
+    for (int i = 0; i < sizeof(coordinates); i++) {
+        coordinates[i] = right.coordinates[i];
+    }
+
+    return *this;
+}
+
+Vector Vector::operator+(const Vector &right) const {
+    return Vector(
+            coordinates[0] + right.coordinates[0],
+            coordinates[1] + right.coordinates[1],
+            coordinates[2] + right.coordinates[2],
+            coordinates[3] + right.coordinates[3],
+            coordinates[4] + right.coordinates[4],
+            coordinates[5] + right.coordinates[5]
+    );
+}
+
+const Vector Vector::operator-(const Vector &right) const {
+    return Vector(
+            coordinates[0] - right.coordinates[0],
+            coordinates[1] - right.coordinates[1],
+            coordinates[2] - right.coordinates[2],
+            coordinates[3] - right.coordinates[3],
+            coordinates[4] - right.coordinates[4],
+            coordinates[5] - right.coordinates[5]
+    );
+}
+
+const Vector Vector::operator*(const Vector &right) const {
+    return Vector(
+            coordinates[1] * right.coordinates[2] - coordinates[2] * right.coordinates[1],
+            coordinates[2] * right.coordinates[0] - coordinates[0] * right.coordinates[2],
+            coordinates[0] * right.coordinates[1] - coordinates[1] * right.coordinates[0],
+            coordinates[4] * right.coordinates[5] - coordinates[5] * right.coordinates[4],
+            coordinates[5] * right.coordinates[3] - coordinates[3] * right.coordinates[5],
+            coordinates[3] * right.coordinates[4] - coordinates[4] * right.coordinates[3]
+    );
+}
+
+bool Vector::operator>(const Vector &right) const {
+    return getMagnitude() > right.getMagnitude();
+}
+
+bool Vector::operator<(const Vector &right) const {
+    return !(*this > right);
+}
+
+bool Vector::operator>=(const Vector &right) const {
+    return getMagnitude() >= right.getMagnitude();
+}
+
+bool Vector::operator<=(const Vector &right) const {
+    return getMagnitude() <= right.getMagnitude();
+}
+
+bool Vector::operator==(const Vector &right) const {
+    return getMagnitude() == right.getMagnitude();
+}
+
+bool Vector::operator!=(const Vector &right) const {
+    return !(*this == right);
+}
+
+//pauze
+
+int main() {
+    Vector v1, v2(1, 2, 3, 4, 5, 6), v3(1, 1, 1, 1, 1, 1);
+
+    cout << "Original values\n"
+         << "v1:\n" << v1 << "\n"
+         << "v2:\n" << v2 << "\n"
+         << "v3:\n" << v3
+        << endl;
+
+    cin >> v1;
+
+    cout << "cin >> v1\n"
+         << "v1:\n" << v1
+         << endl;
+
+    v3 = v1;
+
+    cout << "v3 = v1" << endl;
+    cout << "v3:\n" << v3 << endl;
+
+    cout << "v1 + v2" << endl;
+    cout << (v1 + v2) << endl;
+
+    cout << "v3 - v1" << endl;
+    cout << (v3 - v1) << endl;
+
+    cout << "v1 * v2" << endl;
+    cout << (v1 * v2) << endl;
+
+    cout << "v1 > v2" << endl;
+
+    if (v1 > v2) {
+        cout << "TRUE" << endl;
+        cout << "v1 is greater then v2\n" << endl;
+    } else {
+        cout << "FALSE" << endl;
+        cout << "v1 is smaller or equal to v2\n" << endl;
+    }
+
+    cout << "v1 < v2" << endl;
+
+    if (v1 < v2) {
+        cout << "TRUE" << endl;
+        cout << "v1 is smaller then v2\n" << endl;
+    } else {
+        cout << "FALSE" << endl;
+        cout << "v1 is greater or equal to v2\n" << endl;
+    }
+
+    cout << "v1 >= v2" << endl;
+
+    if (v1 >= v2) {
+        cout << "TRUE" << endl;
+        cout << "v1 is greater or equal to v2\n" << endl;
+    } else {
+        cout << "FALSE" << endl;
+        cout << "v1 is smaller then v2\n" << endl;
+    }
+
+    cout << "v1 <= v2" << endl;
+
+    if (v1 <= v2) {
+        cout << "TRUE" << endl;
+        cout << "v1 is smaller or equal to v2\n" << endl;
+    } else {
+        cout << "FALSE" << endl;
+        cout << "v1 is greater then v2\n" << endl;
+    }
+
+    cout << "v1 == v2" << endl;
+
+    if (v1 == v2) {
+        cout << "TRUE" << endl;
+        cout << "v1 is equal to v2\n" << endl;
+    } else {
+        cout << "FALSE" << endl;
+        cout << "v1 is not equal to v2\n" << endl;
+    }
+
+    cout << "v1 != v2" << endl;
+
+    if (v1 != v2) {
+        cout << "TRUE" << endl;
+        cout << "v1 is not equal to v2\n" << endl;
+    } else {
+        cout << "FALSE" << endl;
+        cout << "v1 is equal to v2\n" << endl;
+    }
+
+    system("PAUSE");
+
+    return 0;
+}
+
